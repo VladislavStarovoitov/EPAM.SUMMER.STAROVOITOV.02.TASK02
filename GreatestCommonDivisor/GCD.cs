88bbead371dd48a827a3ce877dadbe3b4e81ce9a
Vladislav Starovoitov <vladislav.starovoitov94@gmail.com>
2016-07-06 01:07:00 +0300

    Add xml comments and change return value and out param

--- GreatestCommonDivisor/GCD.cs
+++ GreatestCommonDivisor/GCD.cs
@@ -7,73 +7,18 @@ using System.Threading;
 
 namespace GreatestCommonDivisor
 {
+    /// <summary>
+    /// Provides static methods that compute greater common divisor
+    /// </summary>
     public static class GCD
     {
-        public static long EuclideanECDWithTime(out int result, int a, int b)
-        {
-            System.Diagnostics.Stopwatch time = new System.Diagnostics.Stopwatch();
-            time.Start();
-            result = EuclideanECD(a, b);
-            time.Stop();
-            return time.ElapsedTicks;
-        }
-
-        public static long EuclideanECDWithTime(out int result, int a, int b, int c)
-        {
-            System.Diagnostics.Stopwatch time = new System.Diagnostics.Stopwatch();
-            time.Start();
-            result = EuclideanECD(a, b);
-            result = EuclideanECD(result, c);
-            time.Stop();
-            return time.ElapsedTicks;
-        }
-
-        public static long EuclideanECDWithTime(out int result, params int[] numbers)
-        {
-            System.Diagnostics.Stopwatch time = new System.Diagnostics.Stopwatch();
-            time.Start();
-            result = numbers[0];
-            for (int i = 1; i < numbers.Length; i++)
-            {
-                result = EuclideanECD(result, numbers[i]);
-            }
-            time.Stop();
-            return time.ElapsedTicks;
-        }
-
-        public static long BinaryGCDWithTime(out int result, int a, int b)
-        {
-            System.Diagnostics.Stopwatch time = new System.Diagnostics.Stopwatch();
-            time.Start();
-            result = BinaryGCD(a, b);
-            time.Stop();
-            return time.ElapsedTicks;
-        }
-
-        public static long BinaryGCDWithTime(out int result, int a, int b, int c)
-        {
-            System.Diagnostics.Stopwatch time = new System.Diagnostics.Stopwatch();
-            time.Start();
-            result = BinaryGCD(a, b);
-            result = BinaryGCD(result, c);
-            time.Stop();
-            return time.ElapsedTicks;
-        }
-
-        public static long BinaryGCDWithTime(out int result, params int[] numbers)
-        {
-            System.Diagnostics.Stopwatch time = new System.Diagnostics.Stopwatch();
-            time.Start();
-            result = numbers[0];
-            for (int i = 1; i < numbers.Length; i++)
-            {
-                result = BinaryGCD(result, numbers[i]);
-            }
-            time.Stop();
-            return time.ElapsedTicks;
-        }
-
-        private static int EuclideanECD(int a, int b)
+        /// <summary>
+        /// Computes greater common divisor by Euclid algorithm
+        /// </summary>
+        /// <param name="a">first number</param>
+        /// <param name="b">second number</param>
+        /// <returns>Greater common divisor</returns>
+        public static int EuclideanGCD(int a, int b)
         {
             a = a < 0 ? -a : a;
             b = b < 0 ? -b : b;
@@ -89,7 +34,93 @@ namespace GreatestCommonDivisor
             return a;
         }
 
-        private static int BinaryGCD(int a, int b)
+        /// <summary>
+        /// Computes greater common divisor by Euclid algorithm
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <param name="c">Third number</param>
+        /// <returns>Greater common divisor</returns>
+        public static int EuclideanGCD(int a, int b, int c)
+        {
+            int result = EuclideanGCD(a, b);
+            result = EuclideanGCD(result, c);
+            return result;
+        }
+
+        /// <summary>
+        /// Computes greater common divisor by Euclid algorithm
+        /// </summary>
+        /// <param name="numbers">Array of numbers</param>
+        /// <returns>Greater common divisor</returns>
+        public static int EuclideanGCD(params int[] numbers)
+        {
+            int result = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                result = EuclideanGCD(result, numbers[i]);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Computes greater common divisor by Euclid algorithm and counts ticks
+        /// </summary>
+        /// <param name="timeTicks">The variable that stores the ticks of the method execution</param>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>Greater common divisor</returns>
+        public static int EuclideanGCDWithTime(out long timeTicks, int a, int b)
+        {
+            System.Diagnostics.Stopwatch time = new System.Diagnostics.Stopwatch();
+            time.Start();
+            int result = EuclideanGCD(a, b);
+            time.Stop();
+            timeTicks = time.ElapsedTicks;
+            return result;
+        }
+
+        /// <summary>
+        /// Computes greater common divisor by Euclid algorithm and counts ticks
+        /// </summary>
+        /// <param name="timeTicks">The variable that stores the ticks of the method execution</param>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <param name="c">Third number</param>
+        /// <returns>Greater common divisor</returns>
+        public static int EuclideanGCDWithTime(out long timeTicks, int a, int b, int c)
+        {
+            System.Diagnostics.Stopwatch time = new System.Diagnostics.Stopwatch();
+            time.Start();
+            var result = EuclideanGCD(a, b, c);
+            time.Stop();
+            timeTicks = time.ElapsedTicks;
+            return result;
+        }
+
+        /// <summary>
+        /// Computes greater common divisor by Euclid algorithm and counts ticks
+        /// </summary>
+        /// <param name="timeTicks">The variable that stores the ticks of the method execution</param>
+        /// <param name="numbers">Array of numbers</param>
+        /// <returns>Greater common divisor</returns>
+        public static int EuclideanGCDWithTime(out long timeTicks, params int[] numbers)
+        {
+            System.Diagnostics.Stopwatch time = new System.Diagnostics.Stopwatch();
+            time.Start();
+            int result = EuclideanGCD(numbers);
+            time.Stop();
+            timeTicks = time.ElapsedTicks;
+            return result;
+        }
+
+        /// <summary>
+        /// Computes greater common divisor by binary Euclid algorithm
+        /// </summary>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>Greater common divisor</returns>
+        public static int BinaryGCD(int a, int b)
         {
             a = a < 0 ? -a : a;
             b = b < 0 ? -b : b;
@@ -111,6 +142,60 @@ namespace GreatestCommonDivisor
             if (a > b)
                 return BinaryGCD((a - b) >> 1, b);
             return BinaryGCD((b - a) >> 1, a);
+        }
+
+        public static int BinaryGCD(int a, int b, int c)
+        {
+            int result = BinaryGCD(a, b);
+            result = BinaryGCD(result, c);
+            return result;
+        }
+
+        public static int BinaryGCD(params int[] numbers)
+        {
+            int result = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                result = BinaryGCD(result, numbers[i]);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Computes greater common divisor by binary Euclid algorithm and counts tick
+        /// </summary>
+        /// <param name="timeTicks">The variable that stores the ticks of the method execution</param>
+        /// <param name="a">First number</param>
+        /// <param name="b">Second number</param>
+        /// <returns>Greater common divisor</returns>
+        public static int BinaryGCDWithTime(out long timeTicks, int a, int b)
+        {
+            System.Diagnostics.Stopwatch time = new System.Diagnostics.Stopwatch();
+            time.Start();
+            int result = BinaryGCD(a, b);
+            time.Stop();
+            timeTicks = time.ElapsedTicks;
+            return result;
+        }
+
+        public static int BinaryGCDWithTime(out long timeTicks, int a, int b, int c)
+        {
+            System.Diagnostics.Stopwatch time = new System.Diagnostics.Stopwatch();
+            time.Start();
+            int result = BinaryGCD(a, b, c);
+            time.Stop();
+            timeTicks = time.ElapsedTicks;
+            return result;
+        }
+
+        public static int BinaryGCDWithTime(out long timeTicks, params int[] numbers)
+        {
+            System.Diagnostics.Stopwatch time = new System.Diagnostics.Stopwatch();
+            time.Start();
+            int result = BinaryGCD(numbers);
+            time.Stop();
+            timeTicks = time.ElapsedTicks;
+            return result;
         }
 
         private static void Swap(ref int left, ref int right)
